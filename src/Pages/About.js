2b713982272e193a3327a5074ae9c7e8b1daa47c
Franck-maker <foziefranck@gmail.com>
2024-08-09 05:36:09 +0200

    First commit

---
+++ src/Pages/About.js
@@ -0,0 +1,49 @@
+
+import React from "react"
+
+const About = () => {
+    return(
+        <div className= 'mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>Hello, welcome to my blog </h1> 
+        {/* sm: small device, my : margin top and bottom */}
+        <p className="mx-auto leading-relax text-base mb-4">Lorem ipsum dolor sit amet,
+         consectetur adipiscing elit. Phasellus
+         fringilla turpis vel nunc commodo, quis
+         tincidunt nisl tempor. Vivamus nec risus 
+         ac nisi scelerisque tincidunt vel et eros.
+         Proin varius dui sit amet nulla facilisis, nec elementum nunc fermentum. Sed euismod,
+         justo ac cursus ultrices, felis nulla facilisis ligula, et commodo nunc est in lectus.
+         Integer et velit vel nisi tincidunt tincidunt. Ut vitae dui nec urna vestibulum hendrerit.
+         Curabitur id magna ac turpis efficitur sagittis. Integer malesuada lacinia nisl, non varius
+          justo fringilla non.</p>
+          <p className="mx-auto leading-relax text-base mb-4">
+          Lorem ipsum dolor sit amet,
+         consectetur adipiscing elit. Phasellus
+         fringilla turpis vel nunc commodo, quis
+         tincidunt nisl tempor. Vivamus nec risus 
+         ac nisi scelerisque tincidunt vel et eros.
+         Proin varius dui sit amet nulla facilisis, nec elementum nunc fermentum. Sed euismod,
+         justo ac cursus ultrices, felis nulla facilisis ligula, et commodo nunc est in lectus.
+         Integer et velit vel nisi tincidunt tincidunt. Ut vitae dui nec urna vestibulum hendrerit.
+         Curabitur id magna ac turpis efficitur sagittis. Integer malesuada lacinia nisl, non varius
+          justo fringilla non.
+          </p >
+          
+          <p className="mx-auto leading-relax text-base mb-4">
+          Lorem ipsum dolor sit amet,
+         consectetur adipiscing elit. Phasellus
+         fringilla turpis vel nunc commodo, quis
+         tincidunt nisl tempor. Vivamus nec risus 
+         ac nisi scelerisque tincidunt vel et eros.
+         Proin varius dui sit amet nulla facilisis, nec elementum nunc fermentum. Sed euismod,
+         justo ac cursus ultrices, felis nulla facilisis ligula, et commodo nunc est in lectus.
+         Integer et velit vel nisi tincidunt tincidunt. Ut vitae dui nec urna vestibulum hendrerit.
+         Curabitur id magna ac turpis efficitur sagittis. Integer malesuada lacinia nisl, non varius
+          justo fringilla non.
+
+          </p>
+    </div>
+    )
+};
+
+export default About;
---
+++ src/Pages/ArticlesList.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const ArticlesList = () => {
+    return(
+    <div>
+        ArticlesList
+    </div>
+    )
+};
+
+export default ArticlesList;
