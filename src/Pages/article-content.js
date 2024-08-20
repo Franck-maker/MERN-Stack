b450c6916d79905c7f92fe4702fcaf08a6cdcd77
Franck-maker <foziefranck@gmail.com>
2024-08-20 05:43:34 +0200

    setting URL parameters with react router

---
+++ src/Pages/article-content.js
@@ -0,0 +1,77 @@
+/**
+ * this is the dummy content.
+ * the dummy content contains an array with objects which represent
+ * the article.
+ */
+
+const articles = [
+    {
+        name: "learn-react",
+        title: "The Fastest Way to Learn React",
+        thumbnail: "/images/blog1-jpg" ,
+        content: [
+    `Welcome! Today we're going to be talking about the fastest way to learn React.
+    Lorem proin congue ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo.
+    Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, non porta
+    *Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. Nam blandit tristique risus,
+    eget accumsan nisl interdum eu. Aenean ac accumsan nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra.
+    Donec finibus lectus sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor interdum, nibh elit congue tellus,
+    ac vulputate urna lorem nec nisi. Morbi id consequat quam. Vivamus accumsan dui in facilisis aliquet.'
+    Etiam nec lectus urnal Sed sodales ultrices dapibus. Nam blandit tristique risus,
+    eget accumsan nisl interdum eu. Aenean ac accumsan nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus sed lorem ultricies,
+    eget ornare leo luctus. Morbi vehicula, nulla eu tempor interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id consequat quam.
+    Vivamus accumsan dui in facilisis aliquet.`,
+      ],
+    },
+
+    {
+        name: "learn-node",
+        title: "How to Build a Node Server in 10 Minutes",
+        thumbnail: "/images/blog2.jpg",
+        content: [
+    `In this article, we're going to be talking looking at a veny quick way to set up a Node-js server.
+    We'll be discussing some topics such as proin congue ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo.
+    Aliquan malesuada in massa tincidunt egestas. Nam consectetur varius turpis, non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas.
+    Ut eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at sodales purus euismod.*
+    *Donec vel mauris lectus. Etiam nec lectus urna. Sed sodates ultrices dapibus. Nam
+    blandit tristique risus, eget accumsan nisl Anterdun cu. Acnean ac accumsan nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra.
+    Donec finibus lectus sed lorem ultricies,
+    eget ornare leo luctus. Morbi vehicula.
+    nulla eu tempor interdum, nibh elit congue tellus,
+    ac vulputate urna lorem nec nisi. Morbi id consequat quan. Vivamus accumsan dui in
+    facilisis oliquet.
+    •Etiam nec lectus urna. Sed sodales ultrices dapibut Non blandit tristique risus,
+    eget accumsan nisl interdum cu.
+    Aenean ac accumsan nisi. Nunc vel pulvinar diam. Nam
+    eleifend egestas viverra. Donec finibus lectus sed loren ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor interdum,
+    nibh elit congue tellus, ac vulputate urna loren nec nisi. Morbi id consequat quam. Vivanus accumsan dui in facilisis aliquet.`
+       ],
+    },
+    
+    {
+        name: "ny-thoughts-on-learning-react",
+        title: "My Thoughts on Learning Reactis",
+        thumbnail: "/images/blog3.jpg",
+        content:[
+            `Today is the day I talk about something which scares most people: Learning ReactJS.
+    In reality, I'n not sure why people have such a hard time with Learning Reactis Ligula id
+    Lisua posuere, yal gleitend ex eaçasas. Sed in surRis Aco- Allauan malezuada in massa
+    SincAduns eggatas. Nam SONSASTOTHE VACAME SUCRAS, DON ROCER RESU POCASERE DOR- 10
+    sinciduns yulputats nulla quis, casetas. Ut elettend us Apgun non tringalla. Precesat at sedales purus sussmod
+    Donec vel mauris. lectus. Etiam nec lectus, urna.
+    ВООдАК КРАЯЛАЯЧЯ САБЧА.
+    eget assumiRo, nAsl Antergun eu. Aenean ac assumago, niss.
+    pulvinar diam. Nom Cleitend, casstas, viverra. Donec tAntous, Sectus, sed Lorem uSErAR-eS.
+    eget ornare leo luctus.
+    ac vulputate urna tacilAsAs aliquet.'
+    eget assman. лая!. Ansergum, eu. Aenean ac assunsan nisi. Nunc vel
+    elekland. cagatas viverra. Donec tánibus lectua sed loren ultrastes.
+    cact ornare leo
+    Morbi yshicula,
+    tempor interdun, Dibb, elit congue tellus, ae vutputate
+    Sna Loren nec niss. Morb1 1d conseauat Quen Vivanus ascumsan dul in tacilAsts alLquet.`
+        ],
+    },
+];
+    
+export default articles;
